c7bc5e967dc7d18c76e8a57a1a6565aac6e42f19
Ellerey Ireland <ellereyireland1@dyn3207-29.wlan.ic.ac.uk>
2018-10-28 16:21:47 +0000

    Added inflow and outflow to the script.

--- point_generator.py
+++ point_generator.py
@@ -4,12 +4,20 @@ class System:
     def __init__(self, node_number):
         self.nodes = self.generate_nodes(node_number)
         self.distance_matrix = self.calculate_distance_matrix()
+        self.inflow = self.add_inflow()
+        self.outflow = self.add_outflow()
 
     def generate_nodes(self, node_number):
         return np.linspace(0, 100, node_number)
 
     def calculate_distance_matrix(self):
         self.distance_matrix = calculate_1d_dist_matrix(self.nodes)
+        
+    def add_inflow(self):
+        return np.full(len(self.nodes), 10)
+    
+    def add_outflow(self):
+        return np.full(len(self.nodes), 10)
 
 
 def calculate_1d_dist_matrix(positions):
@@ -20,12 +28,12 @@ def calculate_1d_dist_matrix(positions):
         scipy.spatial.distance_matrix
     """
     len_indices = len(positions)
-    distance_matrix = np.zeroes((len_indices, len_indices))
+    distance_matrix = np.zeros((len_indices, len_indices))
 
     index_range = range(len_indices)
 
     for i in index_range:
-        for j in index_range[i,]:
+        for j in index_range[i:]:
             dist = abs(positions[i] - positions[j])
             distance_matrix[i, j] = dist
             distance_matrix[j, i] = dist
