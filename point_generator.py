b9abb6aea5f482212f55673631e8e62cf9f7307d
VinDel77 <vinul25@live.co.uk>
2018-10-28 15:25:38 +0000

    Nothing
    I havent changed anything

--- point_generator.py
+++ point_generator.py
@@ -3,6 +3,10 @@ import numpy as np
 class System:
     def __init__(self, node_number):
         self.nodes = self.generate_nodes(node_number)
+        self.distance_matrix = self.calculate_distance_matrix()
 
     def generate_nodes(self, node_number):
         return np.linspace(0, 100, node_number)
+
+    def calculate_distance_matrix(self):
+        nodes = self.nodes
