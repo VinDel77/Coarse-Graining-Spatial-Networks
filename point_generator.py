f610c2d82b6f9e07eabf1a11f7999bae33176339
VinDel77 <vinul25@live.co.uk>
2018-10-28 15:47:12 +0000

    Produce 1d distance matrix between points

--- point_generator.py
+++ point_generator.py
@@ -9,4 +9,24 @@ class System:
         return np.linspace(0, 100, node_number)
 
     def calculate_distance_matrix(self):
-        nodes = self.nodes
+        self.distance_matrix = calculate_1d_dist_matrix(self.nodes)
+
+
+def calculate_1d_dist_matrix(positions):
+    """
+    Calculate the distance matrix in 1d.
+
+    For 2D  think about using:
+        scipy.spatial.distance_matrix
+    """
+    len_indices = len(positions)
+    distance_matrix = np.zeroes((len_indices, len_indices))
+
+    index_range = range(len_indices)
+
+    for i in index_range:
+        for j in index_range[i,]:
+            dist = abs(positions[i] - positions[j])
+            distance_matrix[i, j] = dist
+            distance_matrix[j, i] = dist
+
