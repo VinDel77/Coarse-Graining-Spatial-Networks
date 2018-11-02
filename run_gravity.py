ff6ac455959cfdc9c89ca92d418919f461f9f257
VinDel77 <vinul25@live.co.uk>
2018-11-02 10:45:37 +0000

    Fixed the tuning function
    - There seems to be an expoenential increase of the A value at the end
    of tuning, not sure why
    - Try and add some noise to the input to see if we can emulate a real
    system a bit better

--- run_gravity.py
+++ run_gravity.py
@@ -32,34 +32,35 @@ class Gravity:
     def tuning_function(self):
         a_values = []
         b_values = []
-        for _ in range(100):
-            new_a = self.calculate_new_a_b(a=True)
+        x_axis = []
+        for _ in range(500):
+            new_a = self.calculate_new_a_b(self.A, self.system.inflow, self.metric, sum_over='i')
             self.A = new_a
-            a_values.append(new_a)
+            a_values.append(new_a[0])
 
-            new_b = self.calculate_new_a_b(a=False)
+            new_b = self.calculate_new_a_b(self.B, self.system.outflow, self.metric, sum_over='j')
             self.B = new_b
-            b_values.append(new_b)
+            b_values.append(new_b[0])
+            x_axis.append(_)
 
         fig = plt.figure()
         ax = fig.add_subplot(111)
 
         print(len(a_values))
         print(len(b_values))
-        x_axis = range(len(a_values))
         ax.plot(x_axis, a_values, 'ro')
         ax.plot(x_axis, b_values, 'bo')
 
         plt.show()
 
-    def calculate_new_a_b(self, a=True):
-        index_range = range(len(self.A))
-        for idx in index_range:
-            if a == True:
-                metric_slice = self.metric[idx, :]
-            else:
-                metric_slice = self.metric[:, idx]
+    def calculate_new_a_b(self, x, f, metric, sum_over='i'):
+        """
+        Calculate the iterated x where x is either A, B .
+        args:
+            x = either A or B as a 1d array
+            f = inflow or outflow as a 1d array
+            metric = metric as a 2D symmetric array
+            sum_over = Sum over row (i) or column (j) of metric
+        """
+        return np.einsum('ij,{}'.format(sum_over), metric, x * f)
 
-            a_dot_b = np.dot(self.A, self.B)
-            inverse_a_b =  a_dot_b * metric_slice
-        return 1.0/inverse_a_b
