2c853519a35855545c769ffbd65f8083e63929e4
VinDel77 <vinul25@live.co.uk>
2018-11-14 22:29:39 +0000

    Implemented a 2d version of coarse graining

--- coarse_graining.py
+++ coarse_graining.py
@@ -11,7 +11,7 @@ class Coarse_graining:
     """
     def __init__(self, system, number_of_areas):
         self.system = system
-        self.boundaries = np.linspace(0, 100 + 1, number_of_areas + 1)
+        self.boundaries = np.linspace(0, 1.01, number_of_areas + 1)
 
     def stack(self, indicies, vector):
         return np.vstack((indicies, vector)).T
@@ -19,7 +19,26 @@ class Coarse_graining:
     def amalgamate(self, bin_vector, func):
         return np.array([func(bin_vector[bin_vector[:, 0] == i, 1]) for i in np.unique(bin_vector[:, 0])])
 
-    def generate_new_system(self):
+    def elements_in_bin(self, nodes_bin_indices, bin_indices, com=False):
+        indices_filter = [np.array_equal(i, bin_indices) for i in nodes_bin_indices]
+        nodes_in_bin = self.system.nodes[indices_filter]
+        inflow_in_bin = self.system.inflow[indices_filter]
+        outflow_in_bin = self.system.outflow[indices_filter]
+
+        inflow = np.sum(inflow_in_bin)
+        outflow = np.sum(outflow_in_bin)
+
+        if com:
+            node_position = self.centre_of_mass(nodes_in_bin, inflow_in_bin + outflow_in_bin)
+        else:
+            node_position = np.mean(nodes_in_bin, axis=0)
+
+        return node_position, inflow, outflow
+
+    def centre_of_mass(nodes, masses):
+        return np.sum(nodes * masses, axis=0) / np.sum(masses)
+
+    def generate_new_system_old(self):
         distance = self.system.nodes
         bins = self.boundaries
         nodes_bin_indicies = np.digitize(distance, bins)
@@ -36,5 +55,29 @@ class Coarse_graining:
         system.set_distance_matrix()
         system.set_inflow(new_inflow)
         system.set_outflow(new_outflow)
+        return system
+
+    def generate_new_system(self, com=True):
+        nodes = self.system.nodes
+        bins = self.boundaries
+
+        nodes_bin_indices = np.digitize(nodes, bins)
+        unique_bins = np.unique(nodes_bin_indices, axis=0)
+
+        new_nodes = []
+        new_inflows = []
+        new_outflows = []
+
+        for uniq in unique_bins:
+            node, inflow, outflow = self.elements_in_bin(nodes_bin_indices, uniq, com=com)
+            new_nodes.append(node)
+            new_inflows.append(inflow)
+            new_outflows.append(outflow)
+
+        system = pg.System()
+        system.set_nodes(np.array(new_nodes))
+        system.set_distance_matrix()
+        system.set_inflow(np.array(new_inflows))
+        system.set_outflow(np.array(new_outflows))
         return system
 
--- plot_nodes.py
+++ plot_nodes.py
@@ -1,8 +1,10 @@
 import matplotlib.pyplot as plt
 
-def plot_nodes(nodes):
-    fig = plt.figure()
+def plot_nodes(nodes, fig=None, fmt='bo'):
+    if fig is None:
+        fig = plt.figure()
+
     ax = fig.add_subplot(111)
 
-    ax.plot(nodes[:, 0], nodes[:, 1], 'bo')
-    plt.show()
+    ax.plot(nodes[:, 0], nodes[:, 1], fmt)
+    return fig
