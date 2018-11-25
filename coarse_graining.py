c102cdf8f0e434b78fd5991b116c1ff2feb32efd
VinDel77 <vinul25@live.co.uk>
2018-11-25 17:16:01 +0000

    Implemented coarse graining which takes flows into account

--- coarse_graining.py
+++ coarse_graining.py
@@ -11,49 +11,88 @@ class Coarse_graining:
     """
     def __init__(self, system, number_of_areas):
         self.system = system
-        self.boundaries = np.linspace(0, 1.01, number_of_areas + 1)
-#        self.total_flow = self.pg.calculate_total_flow()
+        self.boundaries = __calculate_boundaries(number_of_areas)
 
-    def elements_in_bin(self, nodes_bin_indices, bin_indices, com=False):
-        indices_filter = [np.array_equal(i, bin_indices) for i in nodes_bin_indices]
-        nodes_in_bin = self.system.nodes[indices_filter]
-        inflow_in_bin = self.system.inflow[indices_filter]
-        outflow_in_bin = self.system.outflow[indices_filter]
+    def set_system(self, new_sytem):
+        self.system = new_sytem
 
-        inflow = np.sum(inflow_in_bin)
-        outflow = np.sum(outflow_in_bin)
+    def set_number_of_segments(self, number_areas):
+        self.boundaries = __calculate_boundaries(number_areas)
 
-        if com:
-            node_position = self.centre_of_mass(nodes_in_bin, inflow_in_bin + outflow_in_bin)
-        else:
-            node_position = np.mean(nodes_in_bin, axis=0)
+    def get_node_positions(self):
+        return np.digitize(self.system.nodes, self.boundaries)
 
-        return node_position, inflow, outflow
+    def group_node_positions(self, node_positions):
+        """
+        Group the nodes by the common positions in the new coarse grained system.
 
-    def centre_of_mass(self, nodes, masses):
-        return np.dot(masses, nodes) / np.sum(masses)
+        Note they are grouped by their index in system.nodes.
+        """
+        unique_positions = np.unique(node_positions, axis=0)
+        indices = []
 
-    def generate_new_system(self, com=True):
-        nodes = self.system.nodes
-        bins = self.boundaries
+        for i, unique in enumerate(unique_positions):
+            indices.append(np.where((node_positions == unique).all(axis=1))[0])
+        return np.array(indices)
 
-        nodes_bin_indices = np.digitize(nodes, bins)
-        unique_bins = np.unique(nodes_bin_indices, axis=0)
+    def get_new_nodes(self, grouped_indices):
+        new_nodes = np.empty(len(grouped_indices), dtype=np.ndarray)
 
-        new_nodes = []
-        new_inflows = []
-        new_outflows = []
+        for i, indices in enumerate(grouped_indices):
+            nodes = self.system.nodes[indices]
+            mass = self.system.inflow[indices] + self.system.outflow[indices]
+            new_nodes[i] = __combined_position(nodes, mass)
+        return new_nodes
 
-        for uniq in unique_bins:
-            node, inflow, outflow = self.elements_in_bin(nodes_bin_indices, uniq, com=com)
-            new_nodes.append(node)
-            new_inflows.append(inflow)
-            new_outflows.append(outflow)
+    def get_new_flow(self, grouped_indices):
+        sys_length = len(grouped_indices)
+        new_flow = np.zeros((sys_length, sys_length))
+        for i, indices in enumerate(grouped_indices):
+            for j, other_indices in enumerate(grouped_indices):
+                if i == j:
+                    new_flow[i, j] == 0.0
+                else:
+                    new_flow[i, j] = np.sum(self.system.flow_matrix[indices][:, other_indices])
+        return new_flow
+
+    def get_new_inflow(self, new_flow):
+        return np.sum(new_flow, axis=1)
+
+    def get_new_outlfow(self, new_flow):
+        return np.sum(new_flow, axis=0)
+
+    def generate_new_system(self):
+        node_positions = self.get_node_positions()
+        grouped_indices = self.group_node_positions(node_positions)
+
+        new_nodes = self.new_nodes(grouped_indices)
+        new_flow = self.get_new_flow(grouped_indices)
+        new_inflows = self.get_new_inflow(new_flow)
+        new_outflows = self.get_new_outlfow(new_flow)
 
         system = sys.System()
         system.set_nodes(np.array(new_nodes))
         system.set_distance_matrix()
         system.set_inflow(np.array(new_inflows))
         system.set_outflow(np.array(new_outflows))
+        system.set_flow_matrix(new_flow)
         return system
 
+def __calculate_boundaries(number_areas, box_size=1.01):
+        return np.linspace(0, box_size, number_areas + 1)
+
+def __combined_position(nodes, masses=None):
+    if masses is None:
+        return np.mean(nodes, axis=0)
+    return np.dot(masses, nodes) / np.sum(masses)
+
+def combine_flow_matrix(system, indices_bins):
+    sys_length = len(indices_bins)
+    new_flow = np.zeros((sys_length, sys_length))
+    for i, indices in enumerate(indices_bins):
+        for j, other_indices in enumerate(indices_bins):
+            if i == j:
+                new_flow[i, j] == 0.0
+            else:
+                new_flow[i, j] = np.sum(system.flow_matrix[indices][:, other_indices])
+    return new_flow
