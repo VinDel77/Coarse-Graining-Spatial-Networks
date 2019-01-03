546055b6dce9c0db7c3ed527421b82b434995353
VinDel77 <vinul25@live.co.uk>
2019-01-03 18:44:10 +0000

    Reorganise and remove newlines

--- min_cost_d_norm.py
+++ min_cost_d_norm.py
@@ -15,26 +15,26 @@ import matplotlib.pyplot as plt
 from tqdm import tqdm
 import save
 
-for i in tqdm(range(1,101)):
+for i in tqdm(range(1,5)):
     #Make the system
     s = g.Gravity()
     system = sys.System()
     # set normal to False to use zipf distribution for city size
     system.random_system(1000, normal=True)
-    
+
     #set the lists that will contain the data points
     cell_areas = []
     min_cost_values =[]
-    
+
     #set up the original system
     s.set_system(system)
     s.tuning_function()
     s.set_flows()
-    
+
     #Coarse grain the system
     for level in tqdm(range(2,22)):
         distances = []
-        cost_values = []  
+        cost_values = []
         coarse_grainer = cg.Coarse_graining(system, level)
         cell_area = coarse_grainer.get_cell_area()
         cell_areas.append(cell_area)
@@ -44,9 +44,9 @@ for i in tqdm(range(1,101)):
         mean_dist = np.mean(grained_system.distance_matrix)
         bound = np.sqrt(mean_dist)
         distances = list(np.linspace(0.01, mean_dist + 3*bound, 100))
-        
+
         # for each value of distance chosen, rerun the tuning on grained system
-        
+
         for d in distances:
             s.set_system(grained_system, distance =d)
             s.tuning_function()
@@ -55,7 +55,7 @@ for i in tqdm(range(1,101)):
             s.cost_function(original_flows, grained_flows)
             cost_values.append(s.cost)
         min_cost_values.append(distances[np.argmin(cost_values)])
-    
+
     save.save_object(min_cost_values, "cost_value_array_norm_{}".format(i), skip_dialogue = True)
     save.save_object(cell_areas, "cell_area_array_norm_{}".format(i), skip_dialogue = True)
     #fig = plt.figure()
--- optimize_parameter.py
+++ optimize_parameter.py
@@ -15,48 +15,54 @@ import matplotlib.pyplot as plt
 from tqdm import tqdm
 import save
 
+def run_plot():
 #Make the system
-s = g.Gravity()
-system = sys.System()
+    s = g.Gravity()
+    system = sys.System()
 # set normal to False to use zipf distribution for city size
-system.random_system(1000, normal=False)
+    system.random_system(1000, normal=False)
 
 #set the lists that will contain the data points
-distances = []
-cost_values =[]
+    distances = []
+    cost_values =[]
 
 #set up the original system
-s.set_system(system)
-s.tuning_function()
-s.set_flows()
+    s.set_system(system)
+    s.tuning_function()
+    s.set_flows()
 
 #Coarse grain the system
-coarse_grainer = cg.Coarse_graining(system, 5)
+    coarse_grainer = cg.Coarse_graining(system, 5)
 #cell_area = coarse_grainer.get_cell_area()
-grained_system = coarse_grainer.generate_new_system()
-original_flows = grained_system.flow_matrix
+    grained_system = coarse_grainer.generate_new_system()
+    original_flows = grained_system.flow_matrix
 #get the mean value from the distance matrix and set the bounds
-mean_dist = np.mean(grained_system.distance_matrix)
-bound = np.sqrt(mean_dist)
-distances = list(np.linspace(0.01, mean_dist + 5*bound, 1000))
+    mean_dist = np.mean(grained_system.distance_matrix)
+    bound = np.sqrt(mean_dist)
+    distances = list(np.linspace(0.01, mean_dist + 5*bound, 1000))
 
 # for each value of distance chosen, rerun the tuning on grained system
 
-for d in tqdm(distances):
-    s.set_system(grained_system, distance =d)
-    s.tuning_function()
-    s.set_flows()
-    grained_flows = grained_system.flow_matrix
-    s.cost_function(original_flows, grained_flows)
-    cost_values.append(s.cost)
+    for d in tqdm(distances):
+        s.set_system(grained_system, distance =d)
+        s.tuning_function()
+        s.set_flows()
+        grained_flows = grained_system.flow_matrix
+        s.cost_function(original_flows, grained_flows)
+        cost_values.append(s.cost)
 
-#save.save_object(cost_values, "Cost value array")
-fig = plt.figure(1, figsize=(15.0, 9.0))
-plt.rc('text', usetex=True)
-ax = fig.add_subplot(111)
-ax.plot(distances, cost_values, 'r-')
-ax.set_xlabel("Distance coefficient (length)", fontsize = 15)
-ax.set_ylabel("Value of cost function (unitless)", fontsize = 15)
-ax.set_title(r'Cost function value against the parameter d', fontsize = 15)
-plt.savefig('/Users/ellereyireland1/Documents/University/Third_year/BSc_project/Report/Images/cost_function_d')
-plt.show()
+    return distances, cost_values, grained_system, original_flows
+
+
+#if __name__=="__main__":
+#    distances, cost_values, grained_system, original_flows = run_plot()
+##save.save_object(cost_values, "Cost value array")
+#    fig = plt.figure(1, figsize=(15.0, 9.0))
+#    plt.rc('text', usetex=True)
+#    ax = fig.add_subplot(111)
+#    ax.plot(distances, cost_values, 'r-')
+#    ax.set_xlabel("Distance coefficient (length)", fontsize = 15)
+#    ax.set_ylabel("Value of cost function (unitless)", fontsize = 15)
+#    ax.set_title(r'Cost function value against the parameter d', fontsize = 15)
+##plt.savefig('/Users/ellereyireland1/Documents/University/Third_year/BSc_project/Report/Images/cost_function_d')
+#    plt.show()
