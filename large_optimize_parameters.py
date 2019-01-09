75c31073cc6c8a1702316818ffc451b641877a24
VinDel77 <vinul25@live.co.uk>
2019-01-09 22:02:57 +0000

    Fix plots for the final results

--- large_optimize_parameters.py
+++ large_optimize_parameters.py
@@ -58,35 +58,36 @@ sigma_c= pcov[2][2] ** 0.5
 
 x_value = np.linspace(0, max(mean_cell_areas), 1000)
 plt.rc('text', usetex=True)
-fig = plt.figure(1, figsize=(15.0, 9.0))
+plt.rc('font', family='serif', size=18)
+fig = plt.figure(1, figsize=(15, 12))
 ax = fig.add_subplot(111)
 ax.errorbar(mean_cell_areas, norm_mean_optimized_d, yerr=norm_std_optimized_d, fmt= 'k+')
 
-plt.plot(x_value, quadratic(x_value, *popt), c='r', linewidth=1.0,
-             label=r'$f(c) =(%.2E \pm %.2E)d^2 + (%.2E \pm %.2E)d + (%.2E \pm %.2E)$' % (
-                     popt[0], sigma_a, popt[1], sigma_b, popt[2], sigma_c))
-ax.set_xlabel(r'Grained cell area (length$^2$)', fontsize = 15)
-plt.legend(fontsize=15)
-ax.set_ylabel(r'Optimum parameter $d$ (length)', fontsize = 15)
-ax.set_title(r'Normal distribution of optimum $d$ against the grained cell area', fontsize = 15)
+plt.plot(x_value, quadratic(x_value, *popt), c='k', linewidth=1.0)
+ax.set_xlabel(r'Grained cell area $S$ (length$^2$)', fontsize = 20)
+label = r'$f(c) =(%.2E \pm %.0E)d^2 + (%.1E \pm %.0E)d + (%.2E \pm %.0E)$' % ( popt[0], sigma_a, popt[1], sigma_b, popt[2], sigma_c)
+ax.text(0.0, 2.35, label, fontsize=20 )
+ax.set_ylabel(r'Optimum parameter $d_0$ (length)', fontsize = 20)
+ax.set_title(r'Normal distribution of optimum $d_0$ against the grained cell area $S$', fontsize = 20)
+ax.grid(True)
 
 
-#axins = zoomed_inset_axes(ax, 3, loc = 6)
-#axins.tick_params(
-#    axis='both',          # changes apply to the x-axis
-#    which='both',      # both major and minor ticks are affected
-#    bottom=False,      # ticks along the bottom edge are off
-#    top=False,
-#    right=False,     # ticks along the top edge are off
-#    labelbottom=False,
-#    labelleft=False)
-#axins.errorbar(mean_cell_areas, norm_mean_optimized_d, yerr=norm_std_optimized_d, fmt= 'k+')
-#
-#axins.plot(x_value, quadratic(x_value, *popt), c='r', linewidth=1.0)
-#axins.set_xlim(0, 0.05) # apply the x-limits
-#axins.set_ylim(0.45, 0.55)
-#
-#mark_inset(ax, axins, loc1=3, loc2=4, fc="none", ec="0.5")
+axins = zoomed_inset_axes(ax, 3, loc = 6)
+axins.tick_params(
+    axis='both',          # changes apply to the x-axis
+    which='both',      # both major and minor ticks are affected
+    bottom=False,      # ticks along the bottom edge are off
+    top=False,
+    right=False,     # ticks along the top edge are off
+    labelbottom=False,
+    labelleft=False)
+axins.errorbar(mean_cell_areas, norm_mean_optimized_d, yerr=norm_std_optimized_d, fmt= 'k+')
+
+axins.plot(x_value, quadratic(x_value, *popt), c='k', linewidth=1.0)
+axins.set_xlim(0, 0.05) # apply the x-limits
+axins.set_ylim(0.45, 0.55)
+
+mark_inset(ax, axins, loc1=3, loc2=4, fc="none", ec="0.5")
 
 #plt.savefig('/Users/ellereyireland1/Documents/University/Third_year/BSc_project/Report/Images/norm_min_d_parameter')
 
@@ -96,35 +97,34 @@ sigma_a = pcov[0][0] ** 0.5
 sigma_b = pcov[1][1] ** 0.5
 sigma_c= pcov[2][2] ** 0.5
 
-fig = plt.figure(2, figsize=(15.0, 9.0))
+fig = plt.figure(2, figsize=(15, 12))
 ax = fig.add_subplot(111)
 ax.errorbar(mean_cell_areas, zipf_mean_optimized_d, yerr=zipf_std_optimized_d, fmt= 'k+')
 
-plt.plot(x_value, quadratic(x_value, *popt), c='r', linewidth=1.0,
-             label=r'$f(c) =(%.2E \pm %.2E)d^2 + (%.2E \pm %.2E)d + (%.2E \pm %.2E)$' % (
-                     popt[0], sigma_a, popt[1], sigma_b, popt[2], sigma_c))
+plt.plot(x_value, quadratic(x_value, *popt), c='k', linewidth=1.0)
+ax.set_xlabel(r'Grained cell area $S$ (length$^2$)', fontsize = 20)
+label = r'$f(c) =(%.2E \pm %.0E)d^2 + (%.0E \pm %.0E)d + (%.2E \pm %.0E)$' % ( popt[0], sigma_a, popt[1], sigma_b, popt[2], sigma_c)
+ax.text(0.0, 2.35, label, fontsize=20 )
+ax.set_ylabel(r'Optimum parameter $d_0$ (length)', fontsize = 20)
+ax.set_title(r'Zipf distribution of optimum $d_0$ against the grained cell area $S$', fontsize = 20)
+ax.grid(True)
 
-ax.set_xlabel(r'Grained cell area (length$^2$)', fontsize = 15)
-plt.legend(fontsize = 15)
-ax.set_ylabel(r'Optimum parameter $d$ (length)', fontsize = 15)
-ax.set_title(r'Zipf distribution of optimum $d$ against the grained cell area', fontsize = 15)
+axins = zoomed_inset_axes(ax, 3, loc=6)
+axins.tick_params(
+    axis='both',          # changes apply to the x-axis
+    which='both',      # both major and minor ticks are affected
+    bottom=False,      # ticks along the bottom edge are off
+    top=False,
+    right=False,     # ticks along the top edge are off
+    labelbottom=False,
+    labelleft=False)
+axins.errorbar(mean_cell_areas, zipf_mean_optimized_d, yerr=zipf_std_optimized_d, fmt= 'k+')
 
-#axins = zoomed_inset_axes(ax, 3, loc=6)
-#axins.tick_params(
-#    axis='both',          # changes apply to the x-axis
-#    which='both',      # both major and minor ticks are affected
-#    bottom=False,      # ticks along the bottom edge are off
-#    top=False,
-#    right=False,     # ticks along the top edge are off
-#    labelbottom=False,
-#    labelleft=False)
-#axins.errorbar(mean_cell_areas, zipf_mean_optimized_d, yerr=zipf_std_optimized_d, fmt= 'k+')
-#
-#axins.plot(x_value, quadratic(x_value, *popt), c='r', linewidth=1.0)
-#axins.set_xlim(0, 0.05) # apply the x-limits
-#axins.set_ylim(0.45, 0.6)
-#
-#mark_inset(ax, axins, loc1=3, loc2=4, fc="none", ec="0.5")
-#
+axins.plot(x_value, quadratic(x_value, *popt), c='k', linewidth=1.0)
+axins.set_xlim(0, 0.05) # apply the x-limits
+axins.set_ylim(0.45, 0.6)
+
+mark_inset(ax, axins, loc1=3, loc2=4, fc="none", ec="0.5")
+
 #plt.savefig('/Users/ellereyireland1/Documents/University/Third_year/BSc_project/Report/Images/zipf_min_d_parameter')
 plt.show()
